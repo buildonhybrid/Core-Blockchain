9d542c70f8198bbacc619ec1020459da4e22ed3c
Cowchain <139770270+CowchainDev@users.noreply.github.com>
2024-04-09 01:17:39 +0300

    Update node-start.sh

--- node-start.sh
+++ node-start.sh
@@ -93,7 +93,7 @@ startValidator(){
         :
     else
         tmux new-session -d -s node$i
-        tmux send-keys -t 0 "./node_src/build/bin/geth --datadir ./chaindata/node$i --networkid $CHAINID --bootnodes $BOOTNODE --mine --port 326$j --nat extip:$IP --gpo.percentile 0 --gpo.maxprice 100 --gpo.ignoreprice 0 --unlock 0 --password ./chaindata/node$i/pass.txt --syncmode=full console" Enter
+        tmux send-keys -t 0 "./node_src/build/bin/geth --datadir ./chaindata/node$i --networkid $CHAINID --bootnodes $BOOTNODE1,$BOOTNODE2,$BOOTNODE3,$BOOTNODE4 --mine --port 326$j --nat extip:$IP --gpo.percentile 0 --gpo.maxprice 100 --gpo.ignoreprice 0 --snapshot=false --unlock 0 --password ./chaindata/node$i/pass.txt --syncmode=full --verbosity=5 console 2>./logs" Enter
     fi
 
     ((i += 1))
